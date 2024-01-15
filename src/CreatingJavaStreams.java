9e68ba31642b0e34d3252d9ccc278f3121097bbd
Neda bashiri <neda.bashiri.dev@gmail.com>
2024-01-15 23:02:15 +0330

    Update createing-java-streams class and Øaddway to create a stream from specified values

--- src/CreatingJavaStreams.java
+++ src/CreatingJavaStreams.java
@@ -1,2 +1,10 @@
+import java.util.stream.Stream;
+
 public class CreatingJavaStreams {
+    public static void main(String[] args) {
+
+        // Streams from values
+        Stream<String> stream = Stream.of("Java 8 ", "Lambdas ", "In ", "Action");
+
+    }
 }
